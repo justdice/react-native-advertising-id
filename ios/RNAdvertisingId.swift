9340788e4663c7543e3644255d355994d8cad422
Santiago Petrone <santiago@applike.info>
2019-02-18 17:48:14 +0100

    Fix iOS implementation

--- ios/RNAdvertisingId.swift
+++ ios/RNAdvertisingId.swift
@@ -4,42 +4,60 @@
 //  Created by App Like on 28.09.17.
 //
 
-//import AdSupport
+import AdSupport
 //import Foundation
 
 @objc(RNAdvertisingId)
 class RNAdvertisingId: NSObject {
     
-    @objc(getAdvertisingId:)
-    func getAdvertisingId(callback: RCTResponseSenderBlock) -> Void {
-        //
-        //        let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
-        //
+//    @objc(getAdvertisingId:)
+//    func getAdvertisingId(callback: RCTResponseSenderBlock) -> Void {
+//
+//        let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
+//
 //        let response: NSMutableDictionary = [
 //            "isLimitAdTrackingEnabled" : "limit tracking?",
 //            "advertisingId" : "dsa1234"
 //        ]
-        //
-        //        if isAdvertisingTrackingEnabled {
-        //            let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-        //            response["advertisingId"] = idfa
-        //        }
-        callback([NSNull(), "Hello"])
-    }
-    @objc(getAdvertisingId:)
-    static func getAdvertisingId(callback: RCTResponseSenderBlock) -> String {
-        //
-        //        let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
-        //
+//
+//        if isAdvertisingTrackingEnabled {
+//            let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+//            response["advertisingId"] = idfa
+//        }
+//        callback([NSNull(), response])
+//    }
+//    @objc(getAdvertisingId:)
+//    static func getAdvertisingId(callback: RCTResponseSenderBlock) -> String {
+//
+//                let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
+//
 //        let response: NSMutableDictionary = [
 //            "isLimitAdTrackingEnabled" : "limit tracking?",
 //            "advertisingId" : "dsa1234"
 //        ]
-        //
-        //        if isAdvertisingTrackingEnabled {
-        //            let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-        //            response["advertisingId"] = idfa
-        //        }
-        return "Hello2"
+//
+//                if isAdvertisingTrackingEnabled {
+//                    let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+//                    response["advertisingId"] = idfa
+//                }
+//        return "Hello2"
+//    }
+    
+    @objc
+    func getAdvertisingId(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+        let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
+        
+        let response: NSMutableDictionary = [
+            "isLimitAdTrackingEnabled" : isAdvertisingTrackingEnabled,
+            "advertisingId" : ""
+        ]
+        
+        if (isAdvertisingTrackingEnabled) {
+            let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+            response["advertisingId"] = idfa
+        }
+        
+        resolve(response)
     }
+
 }
--- ios/testtt.swift
+++
@@ -1,9 +0,0 @@
-//
-//  testtt.swift
-//  RNAdvertisingId
-//
-//  Created by Santiago Petrone on 08.02.19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
-
-import Foundation
