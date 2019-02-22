2289aacac0e6e0728647b6cc2b816c6557c080c1
Santiago Petrone <santiago@applike.info>
2019-02-22 14:59:45 +0100

    remove comments

--- ios/RNAdvertisingId.swift
+++ ios/RNAdvertisingId.swift
@@ -9,40 +9,6 @@ import AdSupport
 
 @objc(RNAdvertisingId)
 class RNAdvertisingId: NSObject {
-    
-//    @objc(getAdvertisingId:)
-//    func getAdvertisingId(callback: RCTResponseSenderBlock) -> Void {
-//
-//        let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
-//
-//        let response: NSMutableDictionary = [
-//            "isLimitAdTrackingEnabled" : "limit tracking?",
-//            "advertisingId" : "dsa1234"
-//        ]
-//
-//        if isAdvertisingTrackingEnabled {
-//            let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-//            response["advertisingId"] = idfa
-//        }
-//        callback([NSNull(), response])
-//    }
-//    @objc(getAdvertisingId:)
-//    static func getAdvertisingId(callback: RCTResponseSenderBlock) -> String {
-//
-//                let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
-//
-//        let response: NSMutableDictionary = [
-//            "isLimitAdTrackingEnabled" : "limit tracking?",
-//            "advertisingId" : "dsa1234"
-//        ]
-//
-//                if isAdvertisingTrackingEnabled {
-//                    let idfa : String = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-//                    response["advertisingId"] = idfa
-//                }
-//        return "Hello2"
-//    }
-    
     @objc
     func getAdvertisingId(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
         let isAdvertisingTrackingEnabled : Bool = ASIdentifierManager.shared().isAdvertisingTrackingEnabled
@@ -59,5 +25,4 @@ class RNAdvertisingId: NSObject {
         
         resolve(response)
     }
-
 }
